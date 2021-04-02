4310d64ff37d4b40e11d4f79305034b045ecdffb
Developer-felix <onjombafelix1@gmail.com>
2021-04-01 19:08:30 -0700

    User models configurations

--- ecomerceApp/models.py
+++ ecomerceApp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    user_type_choices = ((1, "Admin"), (2, "Staff"), (3, "Merchant"), (4, "Customer"),)
+    user_type = models.CharField(max_length=255, choices=user_type_choices, default=1)
+
+class AdminUser(models.Model):
+    profile_pic = models.FileField(default="")
+    created_at = models.DateTimeField(auto_now_add = True)
+
+class Staff(models.Model):
+    profile_pic = models.FileField(default="")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Merchant(models.Model):
+    profile_pic = models.FileField(default="")
+    company_name = models.CharField(max_length=255)
+    gst_details = models.CharField(max_length=255)
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CustomerUser(models.Model):
+    profile_pic = models.FileField(default="")
+    created_at = models.DateTimeField(auto_now_add = True)  
