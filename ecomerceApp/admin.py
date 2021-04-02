59a573e12e32c3de5d12d305ccce75c59117d689
Developer-felix <onjombafelix1@gmail.com>
2021-04-01 21:31:35 -0700

    Default Admin Panel

--- ecomerceApp/admin.py
+++ ecomerceApp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from ecomerceApp.models import Categories,SubCategories
 
-# Register your models here.
+admin.site.register(Categories)
+admin.site.register(SubCategories)
