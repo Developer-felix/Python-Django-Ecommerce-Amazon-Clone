fb52326def886f308cc19060702f043bec66e9f6
Developer-felix <onjombafelix1@gmail.com>
2021-04-03 14:55:22 -0700

    Static files configurations

--- EcomerceWebsite/settings.py
+++ EcomerceWebsite/settings.py
@@ -120,4 +120,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR,"static")
+
 AUTH_USER_MODEL = "ecomerceApp.CustomUser"
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
