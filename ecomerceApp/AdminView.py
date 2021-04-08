8d9cd71e2826fd0b620f427a36c39515f13cadc7
Developer-felix <onjombafelix1@gmail.com>
2021-04-08 14:27:51 -0700

    Ctegory Create template

--- ecomerceApp/AdminView.py
+++ ecomerceApp/AdminView.py
@@ -7,9 +7,16 @@ from django.contrib.auth.decorators import login_required
 from .models import Categories
 from django.views.generic import CreateView, DeleteView, ListView, DetailView, UpdateView
 
+
 class CategoriesListView(ListView):
     model = Categories
     template_name = "admin_templates/category_list.html"
+
+class CategoriesCreateView(CreateView):
+    model = Categories
+    fields = "__all__"
+    template_name = "admin_templates/category_create.html"
+
 
 @login_required(login_url="/admin/")
 def admin_home(request):
